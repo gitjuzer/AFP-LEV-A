bf6099763cabf8db3a1ed4c39715894f6b263044
Misi <mpekar55@gmail.com>
2018-12-07 16:03:39 +0100

    tananyagok_layout_backend_ 1.rész --előző layoutrol név átadása

---
+++ Source/Android/Project/app/src/main/java/com/main/project/projectx/Curriculum.java
@@ -0,0 +1,11 @@
+package com.main.project.projectx;
+
+public class Curriculum {
+    public Curriculum(String title, String content,int topic){
+
+
+    }
+    String title;
+    String content;
+    int topic;
+}
